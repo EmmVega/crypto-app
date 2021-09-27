73d05523357c61e3bcea116fe32ad2b39ab29e8f
Emmanuel Vega <carlito_sky@hotmail.com>
2021-09-27 14:56:33 -0500

    Chars done

--- src/Components/Cryptocurrencies.jsx
+++ src/Components/Cryptocurrencies.jsx
@@ -1,10 +1,50 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import millify from 'millify';
+import {Link} from 'react-router-dom';
+import {Card, Row, Col, Input} from 'antd';
 
-const Cryptocurrencies = () => {
+import {useGetCryptosQuery} from '../Services/cryptoApi'
+
+const Cryptocurrencies = ({simplyfied}) => {
+    const count = simplyfied ? 10 : 100;
+    const {data: cryptosList, isFetching} = useGetCryptosQuery(count);
+    const [cryptos, setCryptos] = useState(cryptosList?.data?.coins); // not sure what are the qmarks for
+    const [searchTerm, setSearchTerm] = useState('');
+
+    useEffect(() => {
+        const filterData = cryptosList?.data?.coins.filter((coin) => coin.name.toLowerCase().includes((searchTerm.toLocaleLowerCase())));
+
+        setCryptos(filterData);
+    }, [cryptosList, searchTerm]);
+
+    if(isFetching) return 'Loading...';
+
     return (
-        <div>
-            Cryptocurrencies
-        </div>
+        <>
+            {!simplyfied && (
+            <div className='search-crypto'>
+                <Input placeholder='Search coin' onChange={(e) => setSearchTerm(e.target.value)}/>
+            </div>
+            )}
+            <Row gutter={[32,32]} className='crypto-card-container'>
+                {cryptos?.map((currency) => (
+                    <Col xs={24} sm={12} lg={6} className='crypto-card' key={currency.id}>
+                        <Link to={`/crypto/${currency.id}`}>
+                            <Card
+                                title={`${currency.rank}. ${currency.name}`}
+                                extra={<img className='crypto-image' src={currency.iconUrl} />}
+                                hoverable
+                                >
+                                <p>Price: {millify(currency.price)}</p>
+                                <p>MarketCap: {millify(currency.marketCap)}</p>
+                                <p>Daily Change: {millify(currency.change)}</p>
+                            </Card>
+                        </Link>
+                    </Col>
+                ))}
+
+            </Row>
+        </>
     )
 }
 
--- src/Components/Cryptodetails.jsx
+++ src/Components/Cryptodetails.jsx
@@ -1,11 +1,62 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useGetCryptoDetailsQuery, useGetCryptoHistoryQuery } from '../Services/cryptoApi';
+import millify from 'millify';
+import { useParams } from 'react-router';
+import {Col, Select, Row, Typography} from 'antd'
+import LineChart from './LineChart';
+
+const { Title } = Typography;
 
 const Cryptodetails = () => {
+    const {coinId} = useParams();
+    const [timePeriod, setTimePeriod] = useState('7d');
+    const {data, isFetching} = useGetCryptoDetailsQuery(coinId);
+    const {data: coinHistory} = useGetCryptoHistoryQuery({coinId, timePeriod});;
+    const cryptoDetails = data?.data?.coin; 
+    const {Option} = Select;
+
+    const time = ['3h','24h','7d','30d','3m','1y','3y','5y'];
+
+    if (isFetching) return 'Loading...';
     return (
-        <div>
-            Cryptodetails
-        </div>
-    )
+          <Col className='coin-detail-container'>
+              <Col className='coin-heading-container'>
+                  <Title level={2} className='coin-name'>
+                      {cryptoDetails.name} ({cryptoDetails.slug})
+                  </Title>
+                  <p>
+                      {cryptoDetails.name} live price in US dollar 
+                  </p>
+              </Col>
+             <Select
+                defaultValue="7d"
+                className="select-timeperiod"
+                placeholder="Select Time Period"
+                onChange={(value) => setTimePeriod(value)}
+             >
+                {time.map((date) => (
+                   <Option key={date}>{date}</Option>
+                ))}
+             </Select>
+             <LineChart
+                coinHistory={coinHistory}
+                currentPrice={millify(cryptoDetails.price)}
+                coinName={cryptoDetails.name}
+             />
+            <Col className='stats-container'>
+                <Col className='coin-value-statistics'>
+                    <Col className='coin-value-statistics-heading'>
+                        <Title level={3} className='coin-detailes-heading'>
+                            {cryptoDetails.name} value statistics
+                        </Title>
+                        <p>
+                            An overview showing the stats of {cryptoDetails.name}
+                        </p>
+                    </Col>
+                </Col>
+            </Col>
+          </Col>
+    );
 }
 
 export default Cryptodetails;
